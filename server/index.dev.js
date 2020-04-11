9cbf151634905ddc87046ebbcba550d2584c24f2
RynerNO <ryner.no@gmail.com>
2020-04-11 09:06:54 +0300

    d

---
+++ server/index.dev.js
@@ -0,0 +1,55 @@
+import Express from 'express';
+
+import Mongoose from 'mongoose';
+
+import config from '@config';
+
+import v1Router from '@routes';
+
+import Webpack from 'webpack';
+
+import WebpackConfig from '../webpack.config';
+
+import WebpackDevMiddleware from 'webpack-dev-middleware';
+
+import WebpackHotMiddleware from 'webpack-hot-middleware';
+
+import path from 'path';
+
+import BodyParser from 'body-parser';
+
+import compression from 'compression';
+
+Mongoose.connect(config.databaseUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+const app = Express();
+
+app.use(BodyParser.json());
+
+app.use(compression());
+
+const compiler = Webpack(WebpackConfig);
+app.use(
+      WebpackDevMiddleware(compiler, {
+        hot: true,
+        publicPath: WebpackConfig.output.publicPath
+      })
+    );
+      app.use(WebpackHotMiddleware(compiler));
+
+app.use(v1Router);
+
+app.use(Express.static(path.resolve(__dirname, 'public')));
+
+app.get('*', (req, res) => {
+
+    res.sendFile(path.resolve(__dirname, 'public/index.html'));
+  
+  
+  
+});
+app.listen(3000, () => {
+  console.log('run');
+});
