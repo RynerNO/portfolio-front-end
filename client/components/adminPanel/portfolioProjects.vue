6a8dd63394c26fd9d7e56f2be672ab06c57cbebe
RynerNO <ryner.no@gmail.com>
2020-04-16 07:53:08 +0300

    admin panel

--- client/components/adminPanel/portfolioProjects.vue
+++ client/components/adminPanel/portfolioProjects.vue
@@ -1,12 +1,13 @@
 <template lang="pug">
-  div
+  div(:class="$style.portfolioProjectsContainer")
     div(:class='$style.projects')
-      div(:class='$style.project')
-        img
-    button(:class='$style.addPageBtn' @click="addProject") New Project
+      div(:class='$style.project' v-for="(project, index) of projects" :key="index")
+        img(:src="`/site_preview/${project.imageName}`")
+    button(:class='[$style.successButton, $style.addPageBtn]' @click="showModalAddProject = !showModalAddProject") New Project
+
     validation-observer(ref="form" v-slot="{ handleSubmit }")
-      form(:class='$style.addProjectForm' @submit.prevent="handleSubmit(addProject)")
-        p New Project
+      form(v-if="showModalAddProject" :class='$style.addProjectForm' @submit.prevent="handleSubmit(addProject)")
+        p Add Project
         validation-provider(name="name" rules="required" v-slot="{errors}")
           input(name="name"  v-model="newProject.name" type='text' placeholder="Project name")
           span(class="text-red-600 text-align-center text-xl") {{ errors[0] }}
@@ -20,22 +21,22 @@
           input(name="duration"  v-model="newProject.duration" type='text' placeholder="Project duration")
           span(class="text-red-600 text-align-center text-xl") {{ errors[0] }}
         filePond(
-          required="true"
-          ref="projectImage"
-          accepted-file-types="image/jpeg, image/png"
-         labelIdle="'Drag & Drop your project preview image or <span class='filepond--label-action'> Browse </span>'"
-          allow-multiple="false"
-          max-files="1"
-        )
+            required="true"
+            ref="projectImage"
+            accepted-file-types="image/jpeg, image/png"
+          labelIdle="'Drag & Drop your project preview image or <span class='filepond--label-action'> Browse </span>'"
+            allow-multiple="false"
+            max-files="1"
+          )
         filePond(
-          required="true"
-          labelIdle="'Drag & Drop your project or <span class='filepond--label-action'> Browse </span>'"
-          ref="projectArchive"
-          allow-multiple="false"
-          max-files="1"
-        )
-       
-        button(:class='$style.SubmitBtn' type="submit") Submit Project
+            required="true"
+            labelIdle="'Drag & Drop your project or <span class='filepond--label-action'> Browse </span>'"
+            ref="projectArchive"
+            allow-multiple="false"
+            max-files="1"
+          )
+        
+        button(:class='[$style.SubmitBtn, $style.successButton]' type="submit") Submit Project
       
 </template>
 
@@ -43,6 +44,7 @@
 
 
 import axios from "@utils/axios"
+import { GET_PROJECTS } from "@store/projects/actions"
 import { POST_ADD } from "@store/projects/actions"
 import { UNSET_AUTH } from "@store/auth/actions"
 import vueFilePond from 'vue-filepond';
@@ -69,8 +71,6 @@ components: {
   ValidationProvider,
   ValidationObserver,
 },
-
-
 props: {
   auth: {
     type: Object,
@@ -79,9 +79,7 @@ props: {
 },
 data() {
   return {
-    sites: {
-
-    },
+    projects: [],
     newProject: {
       name: '',
       tech: '',
@@ -89,12 +87,15 @@ data() {
       duration: ''
     },
     response: '',
-    error: ''
+    error: '',
+    showModalAddProject: false
    
   }
 },
-mounted() {
-
+created() {
+	  this.$store.dispatch(GET_PROJECTS).then(()=> {
+		this.projects = this.$store.state.projects.projects
+	})
 },
 methods: {
   addProject() {
@@ -112,14 +113,54 @@ methods: {
 </script>
 
 <style lang="scss" module>
-  .sites {
 
+@mixin button {
+  font-size: .9rem;
+  font-weight: 500;
+  padding: .375rem .75rem;
+  user-select: none;
+  transition: color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+  text-align: center;
+  vertical-align: middle;
+  border: 1px solid transparent;
+  border-radius: .25rem;
+  background-color: transparent;
+  color: #fff; 
+  font-family: 'Hind Vadodara', sans-serif;
+}
+.darkButton {
+  @include button;
+  border-color: #32325d;
+  background-color: #32325d;
+  &:hover {
+    color: #fff;
+    border-color: #20203c;
+    background-color: #252544;
   }
-  .site {
-
+}
+.successButton {
+  @include button;
+  border-color: #3ecf8e;;
+  background-color: #3ecf8e;;
+  &:hover {
+    color: #fff;
+    border-color: #2baf74;
+    background-color: #2eb97b;
+  }
+}
+.addPageBtn {
+  align-self: flex-end;
+  margin: 1rem 0;
+}
+  .portfolioProjectsContainer {
+    padding-top: 64px;
+    display: flex;
+    flex-direction: column;
+    width: 90%;
+    margin: 0 auto;
   }
   .addProjectForm {
-    width: 90%;
+    width: 100%;
     margin: 0 auto;
     display: flex;
     flex-direction: column;
